c04acebe81a26c34dc775bb8261b3782913c2d82
cuicanmx <arl96075@163.com>
2025-12-21 20:55:32 +0800

    fix ccache test

--- raphael-kernel_build.sh
+++ raphael-kernel_build.sh
@@ -1,8 +1,9 @@
+
 git clone https://github.com/GengWei1997/linux.git --branch raphael-$1 --depth 1 linux
 cd linux
 wget -P arch/arm64/configs https://raw.githubusercontent.com/GengWei1997/kernel-deb/refs/heads/main/raphael.config
-make -j$(nproc) ARCH=arm64 LLVM=1 defconfig raphael.config
-make -j$(nproc) ARCH=arm64 LLVM=1
+make -j$(nproc) ARCH=arm64 CC="ccache clang" LLVM=1 defconfig raphael.config
+make -j$(nproc) ARCH=arm64 CC="ccache clang" LLVM=1
 _kernel_version="$(make kernelrelease -s)"
 mkdir -p ../linux-xiaomi-raphael/boot/dtbs/qcom
 cp arch/arm64/boot/vmlinuz.efi ../linux-xiaomi-raphael/boot/vmlinuz-$_kernel_version
@@ -10,7 +11,7 @@ cp arch/arm64/boot/dts/qcom/sm8150*.dtb ../linux-xiaomi-raphael/boot/dtbs/qcom
 cp .config ../linux-xiaomi-raphael/boot/config-$_kernel_version
 sed -i "s/Version:.*/Version: ${_kernel_version}/" ../linux-xiaomi-raphael/DEBIAN/control
 rm -rf ../linux-xiaomi-raphael/lib
-make -j$(nproc) ARCH=arm64 LLVM=1 INSTALL_MOD_PATH=../linux-xiaomi-raphael modules_install
+make -j$(nproc) ARCH=arm64 CC="ccache clang" LLVM=1 INSTALL_MOD_PATH=../linux-xiaomi-raphael modules_install
 rm ../linux-xiaomi-raphael/lib/modules/**/build
 cd ..
 rm -rf linux
