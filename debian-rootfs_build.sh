2c44c2a83885309bfba0ff13c878de7e0ef21293
cuicanmx <arl96075@163.com>
2025-12-23 15:29:34 +0800

    refactor: 改进fstab文件生成的格式以提高可读性
    将单行echo命令拆分为多行，使fstab文件内容更清晰易读

--- debian-rootfs_build.sh
+++ debian-rootfs_build.sh
@@ -135,7 +135,8 @@ configure_system() {
     echo "$HOSTNAME" > rootdir/etc/hostname
     echo -e "127.0.0.1\tlocalhost\n127.0.1.1\t$HOSTNAME" > rootdir/etc/hosts
     
-    echo -e "PARTLABEL=userdata / ext4 errors=remount-ro,x-systemd.growfs 0 1\nPARTLABEL=cache /boot vfat umask=0077,nofail 0 1" | tee rootdir/etc/fstab
+    echo "PARTLABEL=userdata / ext4 errors=remount-ro,x-systemd.growfs 0 1
+    PARTLABEL=cache /boot vfat umask=0077,nofail 0 1" | tee rootdir/etc/fstab
 }
 
 configure_network() {
